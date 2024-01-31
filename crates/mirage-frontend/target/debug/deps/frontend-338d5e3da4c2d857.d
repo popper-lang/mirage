ac302349460356aadaf2bb94c41e621977ee6be2
NightProg <tonio.barbier@gmail.com>
2024-01-31 14:51:25 +0100

    Removed crates/mirage-frontend/target from git repository

--- crates/mirage-frontend/target/debug/deps/frontend-338d5e3da4c2d857.d
+++
@@ -1,5 +0,0 @@
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/target/debug/deps/libfrontend-338d5e3da4c2d857.rmeta: src/lib.rs
-
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/target/debug/deps/frontend-338d5e3da4c2d857.d: src/lib.rs
-
-src/lib.rs:
--- crates/mirage-frontend/target/debug/deps/mirage_frontend_module-6157e2fe97b02810.d
+++
@@ -1,7 +0,0 @@
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/target/debug/deps/libmirage_frontend_module-6157e2fe97b02810.rmeta: mirage-frontend-module/src/lib.rs
-
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/target/debug/deps/libmirage_frontend_module-6157e2fe97b02810.rlib: mirage-frontend-module/src/lib.rs
-
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/target/debug/deps/mirage_frontend_module-6157e2fe97b02810.d: mirage-frontend-module/src/lib.rs
-
-mirage-frontend-module/src/lib.rs:
