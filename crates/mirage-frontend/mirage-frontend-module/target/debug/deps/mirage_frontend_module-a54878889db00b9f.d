e8ae0a5cad07980cf0af3748e1bc8ab85e87a512
NightProg <tonio.barbier@gmail.com>
2024-06-16 17:44:42 +0200

    Remove target

--- crates/mirage-frontend/mirage-frontend-module/target/debug/deps/mirage_frontend_module-a54878889db00b9f.d
+++
@@ -1,7 +0,0 @@
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/mirage-frontend-module/target/debug/deps/libmirage_frontend_module-a54878889db00b9f.rmeta: src/lib.rs
-
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/mirage-frontend-module/target/debug/deps/libmirage_frontend_module-a54878889db00b9f.rlib: src/lib.rs
-
-/Users/antoine/Documents/popper_lang/mirage/crates/mirage-frontend/mirage-frontend-module/target/debug/deps/mirage_frontend_module-a54878889db00b9f.d: src/lib.rs
-
-src/lib.rs:
