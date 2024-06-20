cdb47be5336c99602719b5453f6b87beba35bf57
NightProg <tonio.barbier@gmail.com>
2024-06-20 09:32:13 +0200

    change build.rs

--- crates/mirage-backend/mirage-backend-llvm/build.rs
+++ crates/mirage-backend/mirage-backend-llvm/build.rs
@@ -4,7 +4,10 @@ use std::env::set_var;
 static LLVM_VERSION: u64 = 180;
 fn main() {
     if let Ok(llvm_config) = var("MIRAGE_LLVM_PREFIX") {
+        println!("change prefix to {}", llvm_config);
         set_var(format!("LLVM_SYS_{}_PREFIX", LLVM_VERSION), llvm_config);
     }
+
+    println!("cargo:rerun-if-changed=build.rs");
 
 }
